76936bd40bc99e1a32be6edd0ddf440d1063535d
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-09-23 19:40:18 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CurrencyInput from 'react-currency-input';
 
-function App() {
+const App = () => {
+  const [amount, setAmount] = useState('0.00')
+
+  const handleChange = (ev, maskedValue, floatValue) => {
+    setAmount(maskedValue)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CurrencyInput
+          value={amount}
+          onChangeEvent={handleChange}
+          decimalSeparator=","
+          thousandSeparator="."
+          prefix="R$ "
+        />
       </header>
     </div>
   );
